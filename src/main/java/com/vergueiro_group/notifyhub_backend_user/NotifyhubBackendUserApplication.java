596bc85a6188cfb50305a1eb78ae0a90f5a8531f
Vergueirod <diego.ssv@outlook.com>
2025-02-28 19:57:05 -0300

    Initial Commit

---
+++ src/main/java/com/vergueiro_group/notifyhub_backend_user/NotifyhubBackendUserApplication.java
@@ -0,0 +1,13 @@
+package com.vergueiro_group.notifyhub_backend_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyhubBackendUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyhubBackendUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vergueiro_group/notifyhub_backend_user/NotifyhubBackendUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vergueiro_group.notifyhub_backend_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyhubBackendUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
